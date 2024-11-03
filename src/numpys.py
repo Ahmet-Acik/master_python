0b3c1dbdbd888921a0d93f70eb1cd7cd2beb5112
drahmetacik <a.acik@icloud.com>
2024-11-03 10:22:39 +0000

    numpys modified

--- src/numpys.py
+++ src/numpys.py
@@ -25,3 +25,13 @@ print(f"np_mdarray: \n{np_mdarray}")
 print(f"np_mdarray.shape: {np_mdarray.shape}")  # np_mdarray.shape: (3, 3)
 print(f"np_mdarray.ndim: {np_mdarray.ndim}")  # np_mdarray.ndim: 2
 print(f"np_mdarray.size: {np_mdarray.size}")  # np_mdarray.size: 9
+print(f"np_mdarray.dtype : {np_mdarray.dtype}")  # np_mdarray.dtype : int64
+
+np_mdarray = np.array([[1, 2, 3], [4, "Hello", 6], [7, 8, 9]])
+np_mdarray_int = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]] , dtype = np.int64)
+print(f"np_mdarray: \n{np_mdarray.dtype}")# np_mdarray:   <U21
+print(type(np_mdarray[2][2]))  # <class 'numpy.str_'> why not int64 ? because of "Hello" string value in the array 
+print(np_mdarray[2][2])  # 9 
+print(np_mdarray[1][1])  # Hello
+print(np_mdarray[1][2])  # 6
+
